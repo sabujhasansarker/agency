78435c2831cfb24948ae6f552903bb9c925caaa5
abdulhay103 <mdabdulhay95@gmail.com>
2020-06-13 22:30:39 +0600

    Slider is working

--- src/HomePage/slider.js
+++ src/HomePage/slider.js
@@ -1,10 +1,40 @@
 import React from "react";
+import { useState } from "react";
+import { Carousel } from "react-bootstrap";
+import slider1 from "../image/slider1.jpg";
+import slider2 from "../image/slider2.jpg";
+import slider3 from "../image/slider3.jpg";
 
 const Slider = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
   return (
-    <div>
-      <h1>Hello From Slider Page</h1>
-    </div>
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img className="d-block w-100" src={slider1} alt="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={slider2} alt="Second slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={slider3} alt="Third slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 };
 
