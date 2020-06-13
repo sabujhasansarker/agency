62f4138dbe40dba29eb5b0aaca2f989f1b5d393d
abdulhay103 <mdabdulhay95@gmail.com>
2020-06-13 19:29:48 +0600

    Slider Component add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import WhoWeAre from "./components/layout/WhoWeAre";
 import Navbar from "./components/layout/Navbar";
+import Slider from "./HomePage/Slider";
 import { BrowserRouter as Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Route>
       <div className="container">
         <Navbar />
+        <Slider />
         <WhoWeAre />
       </div>
     </Route>
