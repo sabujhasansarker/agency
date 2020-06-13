e29b895e5b9c606959fc88b6037ba70837f89644
sabuj hasan sarker <sabujhasansarker@gmail.com>
2020-06-13 16:37:45 +0600

    navbar ok

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,36 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import "./NavBar.css";
+
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <nav>
-      <div className="row" style={{ alignItems: "center" }}>
-        <div className="col-md-6">
+      <div className="nav_container">
+        <Link to="/">
           <img
             src="https://demo30.atiframe.com/wp-content/uploads/2020/03/Logo_Samura.svg"
             alt=""
             className="logo_img"
           />
+        </Link>
+        <div className="toggle_container">
+          <div className="toggle" onClick={() => setToggle(!toggle)}>
+            {toggle ? (
+              <img
+                src="https://img.icons8.com/ultraviolet/30/000000/delete-sign.png"
+                alt=""
+              />
+            ) : (
+              <img
+                src="https://img.icons8.com/officel/30/000000/menu.png"
+                alt=""
+              />
+            )}
+          </div>
         </div>
-        <div className="col-md-6">
-          <ul className="nav">
-            <li>
-              <Link to="/">Home </Link>
-            </li>
-            <li>
-              <Link to="/services">Services </Link>
-            </li>
-            <li>
-              <Link to="/categories">Categories </Link>
-            </li>
-            <li>
-              <Link to="/about">About Us </Link>
-            </li>
-            <li>
-              <Link to="/contacts">Contacts </Link>
-            </li>
-          </ul>
-        </div>
+        <ul className={`nav ${toggle ? "open_nav" : ""}`}>
+          <li>
+            <Link to="/">Home </Link>
+          </li>
+          <li>
+            <Link to="/services">Services </Link>
+          </li>
+          <li>
+            <Link to="/categories">Categories </Link>
+          </li>
+          <li>
+            <Link to="/about">About Us </Link>
+          </li>
+          <li>
+            <Link to="/contacts">Contacts </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
