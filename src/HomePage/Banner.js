216dc71300bfc099282587adf178ae3955109383
abdulhay103 <mdabdulhay95@gmail.com>
2020-06-14 14:02:17 +0600

    Saparate home page at banner

---
+++ src/HomePage/Banner.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Slider from "./Slider";
+import CounDown from "./CounDown";
+
+const Banner = () => {
+  return (
+    <>
+      <Slider />
+      <CounDown />
+    </>
+  );
+};
+
+export default Banner;
