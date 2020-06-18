11a0e8f68b26956737e751a2c4032b2c05bd7d90
sabuj hasan sarker <sabujhasansarker@gmail.com>
2020-06-19 00:56:45 +0600

    call us

---
+++ src/components/pages/home/CallUs.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import "./CallUs.scss";
+
+const CallUs = () => {
+  return (
+    <div className="callus">
+      <div className="callus_left">
+        <h2>Call us now</h2>
+        <p>
+          The solution that will growth your website traffic up to 2 000% and
+          your website will be scanned and checked for on page.
+        </p>
+        <div className="botton_container">
+          <div className="button">
+            <button>call now</button>
+          </div>
+        </div>
+      </div>
+      <div className="callus_right">
+        <div className="h1">
+          <h1>,,</h1>
+        </div>
+        <h2>WE CREATE THE IDEAS THAT HELP BRANDS</h2>
+        <div className="footer_text">
+          <h4>Alexander Chester</h4>
+          <p>Managing Director</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CallUs;
--- src/components/pages/home/HomeIndex.js
+++ src/components/pages/home/HomeIndex.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from "react";
+
+// Components
 import BannerSlider from "./BannerSlider";
 import WhoWeAre from "../../layout/sabuj/WhoWeAre";
 import Work from "../../layout/sabuj/ourWork/Work";
 import Card from "../../layout/sabuj/Card";
 import Testimonials from "../../layout/sabuj/Testimonials";
+import CallUs from "./CallUs";
 
 const HomeIndex = () => {
   return (
@@ -11,6 +14,7 @@ const HomeIndex = () => {
       <BannerSlider />
       <Card />
       <WhoWeAre />
+      <CallUs />
       <Testimonials background={true} />
       <Work />
     </Fragment>
