24af6f32d26338b8b44cd148d2b5e569c31f46ab
sabuj hasan sarker <sabujhasansarker@gmail.com>
2020-06-19 13:55:05 +0600

    about skill

--- src/components/pages/about/AboutIndex.js
+++ src/components/pages/about/AboutIndex.js
@@ -4,12 +4,14 @@ import Banner from "../../../utils/Banner";
 import WhoWeAre from "../../layout/sabuj/WhoWeAre";
 import Sponser from "../../layout/sabuj/Sponser";
 import Testimonials from "../../layout/sabuj/Testimonials";
+import Skill from "./Skill";
 
 const AboutIndex = () => {
   return (
     <Fragment>
       <Banner>about</Banner>
       <WhoWeAre button={true} />
+      <Skill />
       <Testimonials />
       <Sponser />
     </Fragment>
